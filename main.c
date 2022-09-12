5c2609b9b275ec0b472b33b8fe45004dd78cb651
Mark Black <fmark98@gmail.com>
2022-09-12 10:22:55 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () 
+{
+    int n = 42; // integer
+    unsigned int u_n = 10; // pozitive
+    float f = 3.14;
+    double d = 3.141592; // nagyobb pontosság
+    char c = 'a'; // character
+
+
+    printf("%i %u %f %f %c\n", 
+           n, u_n, f, d, c);
+}
+
+// cppreference.com
+
+/*
+[0|0000000]
+
+
+*/
